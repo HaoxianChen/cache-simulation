207acc8fd8d5cd1b1185436ac89e2ac2a0bc0d0b
Brandon <hxchen414@gmail.com>
2016-11-27 11:04:42 -0500

    re-structed the code, making tirmodal analysis code seperate from compressed MDP value iteration code

--- compressedMDP.py
+++ compressedMDP.py
@@ -1,215 +1,15 @@
 #!/usr/bin/python
 
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import math
 import analytical_model as model
+import trimodal
 import traceGen 
 import cache 
 
 np.set_printoptions(precision=2)
 
-# x1 - size of small array
-# x2 - size of big array
-# p - fraction of accesses to small array
-# d1 = x1 / p
-# d2 = x2 / (1-p)
-#
-# working set size = x1 + x2 = d1 * p + d2 * p = exp(rdd)
-
-def vi_analysis(s):
-    # s: cache size
-    delta_d1 = float(d1*s/ed+1)/float((s+p2)*p1) - p1/p2
-    return delta_d1
-
-def miss_rate_mru(p,d,s):
-    [p1,p2,p3] = p
-    [d1,d2,d3] = d
-    ed = p1*d1 + p2*d2 + p3*d3
-    if s<ed and s>0:
-        miss_rate = 1 - s/ed
-    else:
-        miss_rate = 0
-    return miss_rate
-
-def miss_rate_d1(p,d,s):
-    [p1,p2,p3] = p
-    [d1,d2,d3] = d
-    ed = p1*d1 + p2*d2 + p3*d3
-    if s>0 and s<=d1:
-        miss_rate = 1 - p1*s/d1
-    elif s>d1 and s<ed:
-        miss_rate = (1-p1)*(ed-s)/(ed-d1)
-    else:
-        miss_rate = 0
-
-    return miss_rate
-
-def miss_rate_d2(p,d,s):
-    [p1,p2,p3] = p
-    [d1,d2,d3] = d
-    ed = p1*d1 + p2*d2 + p3*d3
-    critical_d = p1*d1 + (1-p1)*d2
-    if s>0 and s<=critical_d:
-        miss_rate = 1. - (1.-p3)*s/(p1*d1+p2*d2+p3*d2)
-    elif s>critical_d and s<ed:
-        miss_rate = (ed-s)/(d3-d2)
-    else:
-        miss_rate = 0
-
-    return miss_rate
-
-def miss_rate_d2_d1(p,d,s):
-    [p1,p2,p3] = p
-    [d1,d2,d3] = d
-    ed = p1*d1 + p2*d2 + p3*d3
-    critical_d = p1*d1 + (1-p1)*d2
-    if s>0 and s<=d1:
-        miss_rate = 1 - p1*s/d1
-    elif s>d1 and s<=critical_d:
-        miss_rate = 1-p1-p2*(s-d1) / ((1-p1)*(d2-d1))
-    elif s>critical_d and s<ed:
-        miss_rate = (ed-s)/(d3-d2)
-    else:
-        miss_rate = 0
-
-    return miss_rate
-
-def hit_rate_mru(p,d,s):
-    [p1,p2,p3] = p
-    [d1,d2,d3] = d
-    ed = p1*d1 + p2*d2 + p3*d3
-    
-    h = np.zeros(n)
-    e = np.zeros(n)
-
-    if s>0 and s<ed:
-        h[d1] = p1*s/ed
-        h[d2] = p2*s/ed
-        h[d3] = p3*s/ed
-        e[1] = 1-s/ed
-    elif s>=ed:
-        hit_rate = 1.
-        h[d1] = p1
-        h[d2] = p2
-        h[d3] = p3
-
-    return h,e
-
-def hit_rate_d1(p,d,s):
-    [p1,p2,p3] = p
-    [d1,d2,d3] = d
-    ed = p1*d1 + p2*d2 + p3*d3
-    
-    h = np.zeros(n)
-    e = np.zeros(n)
-
-    if s>0 and s<= d1:
-        hit_rate = float(p1*s/ed)
-        h[d1] = hit_rate
-        e[1] = 1. - hit_rate/p1
-        e[d1] = (1. -p1) * hit_rate/p1
-    elif s>d1 and s<ed:
-        hit_rate = 1. - (1. - p1)*(ed-s)/(ed-d1)
-        h[d1] = p1
-        h[d2] = p2*hit_rate/(1. -p1) 
-        h[d3] = p3*hit_rate/(1. -p1) 
-        e[d1] = 1. - hit_rate
-    elif s>=ed:
-        hit_rate = 1.
-        h[d1] = p1
-        h[d2] = p2
-        h[d3] = p3
-
-    return h,e
-
-def hit_rate_d2(p,d,s):
-    [p1,p2,p3] = p
-    [d1,d2,d3] = d
-    ed = p1*d1 + p2*d2 + p3*d3
-    critical_d = p1*d1 + (1-p1)*d2
-
-    h = np.zeros(n)
-    e = np.zeros(n)
-
-    if s>0 and s<= critical_d:
-        hit_rate = (1. - p3)*s/(p1*d1 + (1-p1)*d2)
-        x = hit_rate / (1-p3)
-        h[d1] = p1 * x
-        h[d2] = p2 * x
-        e[1] = 1. - x
-        e[d2] = p3 * x
-    elif s>critical_d and s<ed:
-        hit_rate = 1. - (ed-s)/(d3-d2)
-        h[d1] = p1
-        h[d2] = p2
-        h[d3] = p3 - (1. - hit_rate)
-        e[d2] = 1. - hit_rate
-    elif s>=ed:
-        hit_rate = 1.
-        h[d1] = p1
-        h[d2] = p2
-        h[d3] = p3
-
-    return h,e
-
-def hit_rate_d2_d1(p,d,s):
-    [p1,p2,p3] = p
-    [d1,d2,d3] = d
-    ed = p1*d1 + p2*d2 + p3*d3
-    critical_d = p1*d1 + (1-p1)*d2
-
-    h = np.zeros(n)
-    e = np.zeros(n)
-
-    if s>0 and s<= d1:
-        hit_rate = p1*s/d1
-        x = hit_rate / p1
-        h[d1] = p1 * x
-        e[1] = 1. - x
-        e[d1] = (1-p1) * x
-    elif s>d1 and s<=critical_d:
-        hit_rate = p1 + p2*(s-d1)/((1-p1)*(d2-d1))
-        x = (hit_rate-p1)*(1-p1)/p2
-        h[d1] = p1
-        h[d2] = p2*x/(1-p1)
-        e[d1] = 1-x-p1
-        e[d2] = p3*x/(1-p1)
-    elif s>critical_d and s<ed:
-        hit_rate = 1. - (ed-s)/(d3-d2)
-        h[d1] = p1
-        h[d2] = p2
-        h[d3] = p3 - (1. - hit_rate)
-        e[d2] = 1. - hit_rate
-    elif s>=ed:
-        hit_rate = 1.
-        h[d1] = p1
-        h[d2] = p2
-        h[d3] = p3
-
-    return h,e
-
-def analysis(p,d):
-    _, (a1, a2) = plt.subplots(2,1)
-    a1.set_title('RDD')
-    a1.plot(rdd)
-
-    s = np.arange(1,n)
-
-    a2.set_title('Miss rate')
-    # when 0<S<d1
-    a2.plot([miss_rate_mru(p,d,size) for size in s],label='MRU')
-    a2.plot([miss_rate_d1(p,d,size) for size in s],label='d1')
-    a2.plot([miss_rate_d2(p,d,size) for size in s],label='d2', marker='.')
-    a2.plot([miss_rate_d2_d1(p,d,size) for size in s],label='d2, d1', marker='*')
-    a2.set_xlim(0,ed)
-    a2.set_ylim(0,1)
-    a2.legend(loc='best', fontsize=12)
-    plt.tight_layout()
-    plt.show()
-    plt.close('all')
-    
 def value_iteration(values,p,d,h,e,s,drag,allow_plot=False):
     
     allow_log = True
@@ -230,8 +30,8 @@ def value_iteration(values,p,d,h,e,s,drag,allow_plot=False):
         plt.close('all')
         fig = plt.figure(figsize=(6,6))
         ax = fig.add_subplot(1,1,1)
-        simple = model.analysis_modal(p,d,h,e,False)
-        ax.plot(simple)
+        # simple = model.analysis_modal(p,d,h,e,False)
+        # ax.plot(simple)
         line, = ax.plot(v[0])
         plt.ion()
 
@@ -277,21 +77,6 @@ def value_iteration(values,p,d,h,e,s,drag,allow_plot=False):
 
     return this - this[0]
 
-def opt_policy(p,d,s):
-    miss_rate = np.zeros(4)
-    miss_rate[0] = miss_rate_mru(p,d,s)
-    miss_rate[1] = miss_rate_d1(p,d,s)
-    miss_rate[2] = miss_rate_d2(p,d,s)
-    miss_rate[3] = miss_rate_d2_d1(p,d,s)
-    if np.argmin(miss_rate) == 0:
-        return [0,],miss_rate[0]
-    elif np.argmin(miss_rate) == 1:
-        return [1,],miss_rate[1]
-    elif np.argmin(miss_rate) == 2:
-        return [2,0,1],miss_rate[2]
-    elif np.argmin(miss_rate) == 3:
-        return [2,1,0],miss_rate[3]
-
 def policy_iteration(p,d,s,drag=1):
     ed = round(np.dot(p,d))
     step = - int(ed/8.)
@@ -302,7 +87,7 @@ def policy_iteration(p,d,s,drag=1):
     values = np.arange(max(d))
     for s in cache_size:
 
-        [opt,opt_miss_rate] = opt_policy(p,d,s)
+        [opt,opt_miss_rate] = trimodal.opt_policy(p,d,s)
 
         [h,e] = sim_policy(values,p,d,s)[1:3]
         
@@ -380,25 +165,18 @@ def parse_policy(values,p,d,s):
     policy = np.argsort([values[critical_point[i]+1] for i in range(len(critical_point))])
     
     if policy[0] == 0:
-        miss_rate = miss_rate_mru(p,d,s)
-        h,e = hit_rate_mru(p,d,s)
+        miss_rate = trimodal.miss_rate_mru(p,d,s)
+        h,e = trimodal.hit_rate_mru(p,d,s)
     elif policy[0] == 1:
-        miss_rate = miss_rate_d1(p,d,s)
-        h,e = hit_rate_d1(p,d,s)
+        miss_rate = trimodal.miss_rate_d1(p,d,s)
+        h,e = trimodal.hit_rate_d1(p,d,s)
     elif policy[0] == 2:
         if policy[1] == 0:
-            miss_rate = miss_rate_d2(p,d,s)
-            h,e = hit_rate_d2(p,d,s)
+            miss_rate = trimodal.miss_rate_d2(p,d,s)
+            h,e = trimodal.hit_rate_d2(p,d,s)
         elif policy[1] == 1:
-            miss_rate = miss_rate_d2_d1(p,d,s)
-            h,e = hit_rate_d2_d1(p,d,s)
-
-#     testing convergence
-#     for a in range(1,len(e)):
-#         e[a] += e[a-1] * 0.9
-# 
-#     if np.sum(h) < 1:
-#         e *= (1. - np.sum(h)) / np.sum(e)
+            miss_rate = trimodal.miss_rate_d2_d1(p,d,s)
+            h,e = trimodal.hit_rate_d2_d1(p,d,s)
 
     return miss_rate, h, e
 
@@ -421,7 +199,7 @@ if __name__ == '__main__':
     s = 80
     #assert ed > d2
 
-    analysis(p,d)
+    trimodal.analysis(p,d)
     drag = 0.9999
     values = policy_iteration(p,d,s,drag)
 
